6cbcc6f23b529f78c578637c999e67fd0e099ff7
MichelBoerema <oome.gerrie@gmail.com>
2023-12-13 15:06:21 +0100

    ui

---
+++ assetta jacobs/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject misionsMenu;
+
+    public void PlayGame()
+    {
+        mainMenu.SetActive(false);
+        misionsMenu.SetActive(true);
+    }
+    public void PlayMisionOne()
+    {
+        SceneManager.LoadScene("Michel");
+    }
+}
