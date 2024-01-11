ad276649437dfc2be8e219a5342a5c7918c5ab90
Sjoerd <113519405+DreojsNL@users.noreply.github.com>
2024-01-11 10:52:56 +0100

    boomSHit

--- assetta jacobs/Assets/Scripts/TreeSnowAmmount.cs
+++ assetta jacobs/Assets/Scripts/TreeSnowAmmount.cs
@@ -7,20 +7,11 @@ public class TreeSnowAmount : MonoBehaviour
     public float minSnowAmount = 0.0f;
     public float maxSnowAmount = 1.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // Set a random snow amount at the start
+
         SetRandomSnowAmount();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // You can add any update logic here if needed
-    }
-
-    // Function to set a random snow amount to the object's material
     void SetRandomSnowAmount()
     {
         Renderer renderer = GetComponent<Renderer>();
